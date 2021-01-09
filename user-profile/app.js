79750d2d544bebf32926a0150c5de9ba312433c1
shyam-patel-kira <shyam.p@ahduni.edu.in>
2021-01-09 21:48:19 +0530

    Pushing everything left for the day

--- user-profile/app.js
+++ user-profile/app.js
@@ -4,14 +4,81 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// New stuff to add
+//---------------------------------------------------
+const hbs = require('hbs');
+const MongoClient = require('mongodb').MongoClient;
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+const authUtils = require('./utils/auth');
+const session = require('express-session');
+const flash = require('connect-flash');
+// --------------------------------------------------
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Add new routes
+// --------------------------------------------------
+const authRouter = require('./routes/auth');
+// --------------------------------------------------
+
 var app = express();
+
+// Connect to db
+// --------------------------------------------------
+MongoClient.connect('mongodb://localhost', (err, client) => {
+  if (err) {
+    throw err;
+  }
+
+  const db = client.db('account-app');
+  const users = db.collection('users');
+  app.locals.users = users;
+});
+// --------------------------------------------------
+
+
+// Configure passport
+// --------------------------------------------------
+passport.use(new Strategy(
+  (username, password, done) => {
+    app.locals.users.findOne({ username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      }
+
+      if (user.password != authUtils.hashPassword(password)) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, { id });
+});
+// --------------------------------------------------
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// Set partials for handlebars
+// --------------------------------------------------
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+// --------------------------------------------------
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +86,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Configure session, passport, flash
+// --------------------------------------------------
+app.use(session({
+  secret: 'session secret',
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.loggedIn = req.isAuthenticated();
+  next();
+});
+// --------------------------------------------------
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Add new routes
+// --------------------------------------------------
+app.use('/auth', authRouter);
+// --------------------------------------------------
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
