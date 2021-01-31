96eb5ad9f58429248e043c9c42d925d4ce698d6d
shyam-patel-kira <shyam.p@ahduni.edu.in>
2021-01-31 18:19:29 +0530

    Added Valid Email constraints for feedback service

--- feedback/Feedback.js
+++ feedback/Feedback.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 // function validateEmail(email) {
 //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 //     return re.test(String(email).toLowerCase());
-//  match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+//  
 //  }
 
 mongoose.model("Feedback", {
@@ -25,7 +25,8 @@ mongoose.model("Feedback", {
     },
     email: {
         type: String,
-        require: true
+        require: true,
+        match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     }
 
 });
