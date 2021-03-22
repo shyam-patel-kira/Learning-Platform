8686dbce593fa1e941aa067b0ca616353f20023c
Shyam Patel <shyam.p@ahduni.edu.in>
2021-03-22 19:26:37 +0530

    Integration Testing

--- src/Components/Ielts/Reading/Ieltsreadingtest.js
+++ src/Components/Ielts/Reading/Ieltsreadingtest.js
@@ -6,21 +6,33 @@ class Ieltsreadingtest extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            
+//            results : {}
         }
     }
 
     //(x[x.length - 1])
     componentDidMount() {
         var x = window.location.href.split('/')
-        axios.get('')
-            .then()
+        console.log(x[x.length - 1]);
+        axios.get(`http://localhost:8000/ielts/reading/test/${x[x.length - 1]}`)
+            .then(
+                res => {
+                    console.log(res.data);
+                    this.setState ({
+                        results: res.data
+                    })
+                    }
+                )
+
     }
 
     render(){
         return (
             <div>
                 <p>Hii</p>
+                <p>
+                    {this.state.results}
+                </p>
             </div>
         )
     }
