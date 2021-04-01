7b5bf65bafec6e1d83fc58b524acd4e97105980f
shantanu2306 <shethshantanu23@gmail.com>
2021-04-01 17:07:41 +0530

    Updated Test1 CSS

--- frontend/src/Components/Ielts/Reading/Ieltsreadingtest.js
+++ frontend/src/Components/Ielts/Reading/Ieltsreadingtest.js
@@ -4,21 +4,40 @@ import './Ieltsreadingtest.css';
 // import { Wrapper, Grid, Content } from "./styles";
 import { Img } from 'react-image';
 
+
+
 class Ieltsreadingtest extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
            passages : [],
-           questions : []
+           questions : [],
+           testid:'',
+           answers:{}
         }
     }
 
-    //(x[x.length - 1])
+    handleChange = (e)  => {
+        let answers = this.state.answers;
+        answers[e.target.id] = e.target.value;
+        this.setState({
+          answers
+        });
+      }
+
+      handleSubmit = (e) => {
+          e.preventDefault()
+          console.log(this.state.answers)
+      }
+
     componentDidMount() {
+        
         console.log("componentdidMount")
         let x = window.location.href.split('/')
-        console.log(x[x.length - 1]);
+        let mytest = x[x.length - 1]
+        
+        
         axios.get(`http://localhost:8000/ielts/reading/test/${x[x.length - 1]}`)
             .then(
                 res => {
@@ -27,9 +46,11 @@ class Ieltsreadingtest extends React.Component {
                         passages: res.data.results[0],
                         questions: res.data.results[0].questions
                     })
-                    }
+                }
                 )
-
+                this.setState({
+                    testid: mytest
+                })
     }
 
     render() {
@@ -39,35 +60,66 @@ class Ieltsreadingtest extends React.Component {
           if (i.includes('Passage')) {
             y.push(
                 <div className='passage-content'>
-            <h2> Hii{i}</h2>
+            <h2> {i}</h2>
             </div>
             );
             Object.values(this.state.passages[i]).map((z) => y.push(<p>{z}</p>));
           }
         }
 
-        console.log(this.state.questions);
+        let text = [];
+        for (let i = 1; i <= 40; i++) {
+          text.push(
+            <label
+            style={{color:'#fff'}}
+            >
+              {i}
+              <input 
+              style={{ display:'block', margin:'10px', width:'120px', marginRight:'20px', border:'3px solid'}}
+              type="text" 
+              id={i.toString()} 
+              key={i.toString()}
+              onChange={(e) => {
+              this.handleChange(e)
+              }} />
+            </label>
+          );
+        }
+        text.push(
+            
+            <button className='btn-style'
+              type="submit"
+              onClick={(e) => {
+                this.handleSubmit(e);
+                for (let i = 1; i <= 40; i++) {
+                  (document.getElementById(i.toString())).value = '';
+                }
+              }}
+            >
+              Submit
+            </button>
+        )
 
         return (
             <div className='main-container'>
                 <div className='passage-container'>
-                    <h1>Test1</h1>
+                    <h1>Test-{this.state.testid}</h1>
                     <div className='passage-content-1'>
                     {y}
-                    {/* <img src={x} alt="404 Image not found"/> */}
                     </div>
-                    <Img
-                        src={this.state.questions.imgurl_1}
-                    />
-                    
-                    
-                    <button className='submit-test'
-                        type='submit'
-                        // onClick={this.feedBackSubmit}
-                    >
-                    Submit
-                    </button>
                 </div>
+                <div className='image-container'>
+                    <Img src={this.state.questions.imgurl_1} />
+                    <Img src={this.state.questions.imgurl_2} />
+                    <Img src={this.state.questions.imgurl_3} />
+                    <Img src={this.state.questions.imgurl_4} />
+                    <Img src={this.state.questions.imgurl_5} />
+                    <Img src={this.state.questions.imgurl_6} />
+                    </div>
+                <div className='textbox-container'>
+                    {text}
+                </div>
+                
             </div>
         )
       }
---
+++ frontend/src/Components/Ielts/Reading/Readinganswersdata.js
@@ -0,0 +1,8 @@
+export const answersdata = [
+
+    {
+        id="1",
+        type 
+    }
+
+]
