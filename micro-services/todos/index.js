29ce134554e8daab5cf218bc95bf7338a92f37be
Shyam Patel <shyam.p@ahduni.edu.in>
2021-05-03 23:05:44 +0530

    Todos service init

--- micro-services/todos/index.js
+++ micro-services/todos/index.js
@@ -5,30 +5,36 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
 //Server Optimisation
-
+const compression = require(`compression`);
+//config file
 const dotenv = require(`dotenv`)
 dotenv.config();
 // All Routes
+const todos_routes = require("./Routes/todos.js");
 
-const JWT_SECRET = process.env.TOKEN_SECRET
-const JWT_SECRET_ADMIN = process.env.TOKEN_SECRET_ADMIN
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
+const todosRouter = express();
 
 // Connect to MongoDB
+//console.log(process.env.DB_CONNECT);
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(process.env.DB_CONNECT, () => {
-  console.log("Database connected to Todos Service sucessfully!");
+  console.log("Database connected to todos Service sucessfully!");
 })
 
+todosRouter.use(bodyParser.json());
+todosRouter.use(compression());
+todosRouter.use(cors());
+todosRouter.use(express.json());
+todosRouter.use("/todos", todos_routes);
 
-app.listen(6545, () => {
-console.log("Up and running! This is our Todos service");
+
+todosRouter.use('/', (req, res,next)=>{
+   res.send('<h1> You have reached our middleware. Maybe u r lost. </h1>');
+});
+
+todosRouter.listen(6545, () => {
+console.log("Up and running! This is our todos service");
 })
